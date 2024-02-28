71a2760f95671ff0d27ff095b28df896fa5bbb69
wwartick <willwartick@gmail.com>
2024-02-28 09:18:50 -0800

    js for menus

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const doStuff = function(e) {
     const target=e.target.closest('div');
     const activeEl = document.querySelectorAll('.active');
+    const activeMenuEl = document.querySelectorAll('.active-menu');
     activeEl.forEach((element) => element.classList.remove('active'));
+    activeMenuEl.forEach((element) => element.classList.remove('active-menu'));
 
     if (target.classList.contains('sort') ||
         target.classList.contains('filters') ||
@@ -9,7 +11,25 @@ const doStuff = function(e) {
             target.classList.toggle('active');
     }
 
+    if(target.classList.contains('sort')){
+        const firstBox = document.getElementById('1');
+        firstBox.classList.toggle('active-menu');
+    }
+
+    if(target.classList.contains('filters')){
+        const secondBox = document.getElementById('2');
+        secondBox.classList.toggle('active-menu');
+    }
+
+    if(target.classList.contains('categories')){
+        const thirdBox = document.getElementById('3');
+        thirdBox.classList.toggle('active-menu');
+    }
+
+
     activeEl.forEach((element) => element.classList.remove('active'));
+    activeMenuEl.forEach((element) => element.classList.remove('active-menu'));
+
    
 }
 
